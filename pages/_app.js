6bc8923ced7d21e77b4a9b2ee5569d0fc28783c0
Samuel Henrique Oliveira da Silva <samosaara@gmail.com>
2020-12-12 11:20:18 -0300

    feat: basic formatting and wip landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import React from 'react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { device } from '../lib/breakpoints'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 14px;
+    @media ${device.laptop} {
+      font-size: 16px;
+    }
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: aliceblue;
+    align-items: center;
+    background-image: url('coin_spin.gif');
+    background-position: center;
+    background-size: 7vmax;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
 }
 
-export default MyApp
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,36 @@
+import React from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+
+const Titulo = styled.h1`
+  font-family: 'Georgia', serif;
+  background-color: pink;
+  padding: 5px 15px;
+  margin: 0 10px;
+  font-size: 4rem;
+`
+
+const WipMark = styled.h5`
+  font-family: 'Comic Sans MS', 'Roboto', sans-serif;
+  width: fit-content;
+  background-color: aqua;
+  font-size: 1.5rem;
+  padding: 5px 15px;
+  margin: 0 20px;
+  margin-left: auto;
+`
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div>
+        <Titulo>SAMUEL HNRQ!</Titulo>
+        <WipMark>Work in progress!</WipMark>
+      </div>
     </div>
   )
 }
