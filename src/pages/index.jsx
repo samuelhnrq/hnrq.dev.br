a87f120996f495adb794a27219f112b2efb807af
Samuel Henrique Oliveira da Silva <samosaara@gmail.com>
2020-12-13 11:50:57 -0300

    new title & favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>HNRQ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <TiltedContainer>
